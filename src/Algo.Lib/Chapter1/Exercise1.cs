bab52c9196ec9edf15b6db0eec39cefb8191c31a
Oleksandr_Melnyk2 <Oleksandr_Melnyk2@epam.com>
2017-07-26 09:59:38 +0300

    Completed exercise #1

---
+++ src/Algo.Lib/Chapter1/Exercise1.cs
@@ -0,0 +1,30 @@
+﻿namespace Algo.Lib.Chapter1
+{
+    public class Exercise1
+    {
+        public static bool IsUniqueChar(string str)
+        {
+            // In the csharp language strings have UTF-16 encoding by default
+            if (str.Length > 0xFFFF)
+            {
+                return false;
+            }
+
+            int[] checker = new int[2048];
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                int rnk = str[i] / 32;
+                int pos = str[i] % 32;
+
+                if ((checker[rnk] & (1 << pos)) > 0)
+                {
+                    return false;
+                }
+                checker[rnk] |= (1 << pos);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ tests/Algo.Lib.Test/Chapter1/Exercise1Test.cs
@@ -0,0 +1,29 @@
+﻿namespace Algo.Lib.Test.Chapter1
+{
+    using Lib.Chapter1;
+    using Xunit;
+
+    public class Exercise1Test
+    {
+        [Fact]
+        public void Check_an_unique_string()
+        {
+            var str = "ABCabc";
+
+            var res = Exercise1.IsUniqueChar(str);
+
+            Assert.True(res);
+        }
+
+        [Fact]
+        public void Check_an_ununique_string()
+        {
+            var str = "ABCAbc";
+
+            var res = Exercise1.IsUniqueChar(str);
+
+            Assert.False(res);
+        }
+
+    }
+}
