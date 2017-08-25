7e18f9111ae0f75735a0e23a1ecd906c48221c2c
Oleksandr_Melnyk2 <Oleksandr_Melnyk2@epam.com>
2017-08-25 18:16:41 +0300

    ch3(ex1)

---
+++ src/Algo.Lib/Chapter3/Exercise1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algo.Lib.Chapter3
+{
+    public class StackArray
+    {
+        private readonly int _size;
+        private readonly int[] _buffer;
+        private readonly int[] _pointer;
+
+        public StackArray(int size)
+        {
+            _size = size;
+            _buffer = new int[_size * 3];
+            _pointer = new int[] { 0, 0, 0 };
+
+        }
+
+
+        public void Push(int stackNum, int value)
+        {
+            if (_pointer[stackNum] >= _size)
+            {
+                throw new Exception("The stack doesn't have enought space");
+            }
+            int idx = stackNum * _size + _pointer[stackNum] + 1;
+            _pointer[stackNum]++;
+            _buffer[idx] = value;
+        }
+
+        public int Pop(int stackNum)
+        {
+            if (_pointer[stackNum] == 0)
+            {
+                throw new Exception("The stack is empty");
+            }
+            int idx = stackNum * _size + _pointer[stackNum];
+            _pointer[stackNum]--;
+            return _buffer[idx];
+        }
+
+        public int Peek(int stackNum)
+        {
+            int idx = stackNum * _size + _pointer[stackNum];
+            return _buffer[idx];
+        }
+
+    }
+
+    public class Exercise1
+    {
+        public static void Push(StackArray stack, int stackNum, int value)
+        {
+            stack.Push(stackNum, value);
+        }
+
+        public static int Pop(StackArray stack, int stackNum)
+        {
+            return stack.Pop(stackNum);
+        }
+
+        public static int Peek(StackArray stack, int stackNum)
+        {
+            return stack.Peek(stackNum);
+        }
+    }
+}
---
+++ tests/Algo.Lib.Test/Chapter3/Exercise1Test.cs
@@ -0,0 +1,42 @@
+﻿namespace Algo.Lib.Test.Chapter3
+{
+    using Lib.Chapter3;
+    using Xunit;
+
+    public class Exercise1Test
+    {
+        [Fact]
+        public void Try_to_add_into_stack()
+        {
+            var stack = new StackArray(5);
+
+            stack.Push(0, 1);
+            stack.Push(0, 2);
+            stack.Push(0, 3);
+
+            stack.Push(1, 11);
+            stack.Push(1, 12);
+            stack.Push(1, 13);
+
+            stack.Push(2, 21);
+            stack.Push(2, 22);
+            stack.Push(2, 23);
+
+
+            var value0 = stack.Pop(0);
+            var value1 = stack.Pop(1);
+            var value2 = stack.Pop(2);
+            var value3 = stack.Pop(2);
+            var value4 = stack.Pop(2);
+            var value5 = stack.Pop(1);
+
+
+            Assert.Equal(value0, 3);
+            Assert.Equal(value1, 13);
+            Assert.Equal(value2, 23);
+            Assert.Equal(value3, 22);
+            Assert.Equal(value4, 21);
+            Assert.Equal(value5, 12);
+        }
+    }
+}
