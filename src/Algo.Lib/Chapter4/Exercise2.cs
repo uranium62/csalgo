7249d8f48292c8b9855d34c37b72682f072e6251
Alexander Melnik <moris912@gmail.com>
2017-09-11 09:25:47 +0300

    ch4(ex2)

--- src/Algo.Lib/Chapter4/Exercise2.cs
+++ src/Algo.Lib/Chapter4/Exercise2.cs
@@ -17,8 +17,11 @@
 
         public List<GraphNode<T>> Nodes { get; set; }
 
-        public GraphNode(T data) : this(data, null)
+        public GraphNode(T data) 
         {
+            Value = data;
+            Nodes = null;
+            State = NodeState.Unvisited;
         }
 
         public GraphNode(T data, List<GraphNode<T>> nodes)
@@ -26,6 +29,10 @@
             Value = data;
             Nodes = nodes;
             State = NodeState.Unvisited;
+        }
+
+        public GraphNode(T data, params GraphNode<T>[] nodes) : this(data, new List<GraphNode<T>>(nodes))
+        {  
         }
 
         public void Reset()
@@ -44,16 +51,69 @@
 
     public class Exercise2
     {
-        public static void DepthSearch(GraphNode<int> graph, GraphNode<int> start, GraphNode<int> end)
+        public static bool DepthSearch(GraphNode<int> graph, GraphNode<int> start, GraphNode<int> end)
         {
             graph.Reset();
 
             var stack = new Stack<GraphNode<int>>();
+
+            start.State = NodeState.Visiting;
+            stack.Push(start);
+
+            while (stack.Count != 0)
+            {
+                GraphNode<int> current = stack.Pop();
+
+                if (current != null && current.Nodes != null)
+                {
+                    foreach (var node in current.Nodes)
+                    {
+                        if (node.State == NodeState.Unvisited)
+                        {
+                            if (node == end)
+                                return true;
+
+                            node.State = NodeState.Visiting;
+                            stack.Push(node);
+                        }
+                    }
+                }
+            }
+
+            return false;
         }
 
-        public static void WidthSearch(GraphNode<int> graph, GraphNode<int> start, GraphNode<int> end)
+        public static bool WidthSearch(GraphNode<int> graph, GraphNode<int> start, GraphNode<int> end)
         {
             graph.Reset();
+
+            var queue = new Queue<GraphNode<int>>();
+
+            start.State = NodeState.Visiting;
+            queue.Enqueue(start);
+
+            while (queue.Count != 0)
+            {
+                GraphNode<int> current = queue.Dequeue();
+
+                if (current != null && current.Nodes != null)
+                {
+                    foreach (var node in current.Nodes)
+                    {
+                        if (node.State == NodeState.Unvisited)
+                        {
+                            if (node == end)
+                                return true;
+
+                            node.State = NodeState.Visiting;
+                            queue.Enqueue(node);
+                        }
+                    }
+                    current.State = NodeState.Visited;
+                }
+            }
+
+            return false;
         }
     }
 }
