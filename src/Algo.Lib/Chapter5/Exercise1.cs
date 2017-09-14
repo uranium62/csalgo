aa1fa341ff499fe21d6996daddfee5ea71af05f3
Alexander Melnik <moris912@gmail.com>
2017-09-14 09:46:09 +0300

    ch5(ex1)

---
+++ src/Algo.Lib/Chapter5/Exercise1.cs
@@ -0,0 +1,19 @@
+﻿namespace Algo.Lib.Chapter5
+{
+    public class Exercise1
+    {
+        public static int ReplaceBits(int n, int m, int i, int j)
+        {
+            int allones = ~0;
+            int left = allones << (j + 1);
+            int right = (1 << i) - 1;
+
+            int mask = left | right;
+
+            int n_cleaned = n & mask;
+            int m_shifted = m << i;
+
+            return n_cleaned | m_shifted;
+        }
+    }
+}
---
+++ tests/Algo.Lib.Test/Chapter5/Exercise1Test.cs
@@ -0,0 +1,20 @@
+﻿namespace Algo.Lib.Test.Chapter5
+{
+    using Lib.Chapter5;
+    using Xunit;
+
+    public class Exercise1Test
+    {
+        [Fact]
+        public void Replace_bits()
+        {
+            int n = 0b100_0000_0000;
+            int m = 0b1_0011;
+            int i = 2, j = 6;
+
+            var r = Exercise1.ReplaceBits(n, m, i, j);
+
+            Assert.Equal(r, 0b100_0100_1100);
+        }
+    }
+}
